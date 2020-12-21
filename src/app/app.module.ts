a4712c00527b8ab16a8daf6a210cddeca8cde3be
cristopxer <cristopherortiz457@gmail.com>
2020-12-20 21:20:09 -0600

    router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,9 +14,20 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component'
+
+import {DishService}  from './services/dish.service';
+
+
+
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent, AboutComponent, HomeComponent, ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,7 +39,7 @@ import { MenuComponent } from './menu/menu.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared/dish.interface.ts
+++ src/app/shared/dish.interface.ts
@@ -1,10 +1,13 @@
+import {Comment} from '../shared/comment.interface'
+
 export interface Dish {
-    id: string;
-    name: string;
-    image: string;
-    category: string;
-    featured: boolean;
-    label: string;
-    price: string;
-    description: string;
+    id?: string;
+    name?: string;
+    image?: string;
+    category?: string;
+    featured?: boolean;
+    label?: string;
+    price?: string;
+    description?: string;
+    comments?: Comment[];
 }
